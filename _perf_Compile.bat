e1daf8f55896bd7dbb2a108d9c2bf4e619465e53
lionel.gueganton <lionel.gueganton@60e0f196-961e-0410-a92f-9d946f01d26a>
2006-09-29 14:17:34 +0000

    Removed file/folder
    git-svn-id: http://abc4j.googlecode.com/svn/trunk@4 60e0f196-961e-0410-a92f-9d946f01d26a

--- _perf_Compile.bat
+++
@@ -1,1 +0,0 @@
-C:\j2sdk1.4.2_05\bin\javac -classpath .\abc4j.jar PerfMeasurements.java
--- _perf_Run.bat
+++
@@ -1,1 +0,0 @@
-C:\j2sdk1.4.2_05\bin\java -classpath .\abc4j.jar;. PerfMeasurements "C:/MyStuff/partitions/ABC/Reels.abc" 2
