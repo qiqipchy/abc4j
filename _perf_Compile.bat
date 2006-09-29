2d2cfbae745659417b574235be9e20f9a84e2141
lionel.gueganton <lionel.gueganton@60e0f196-961e-0410-a92f-9d946f01d26a>
2006-09-29 13:57:34 +0000

    git-svn-id: http://abc4j.googlecode.com/svn/trunk@2 60e0f196-961e-0410-a92f-9d946f01d26a

---
+++ _perf_Compile.bat
@@ -0,0 +1,1 @@
+C:\j2sdk1.4.2_05\bin\javac -classpath .\abc4j.jar PerfMeasurements.java
---
+++ _perf_Run.bat
@@ -0,0 +1,1 @@
+C:\j2sdk1.4.2_05\bin\java -classpath .\abc4j.jar;. PerfMeasurements "C:/MyStuff/partitions/ABC/Reels.abc" 2
