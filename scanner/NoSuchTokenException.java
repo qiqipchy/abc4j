d1ecfc6cc5cd6d64a680607a1b02fc0b953d45b8
lionel.gueganton <lionel.gueganton@60e0f196-961e-0410-a92f-9d946f01d26a>
2006-10-04 14:31:15 +0000

    Moved remotely
    git-svn-id: http://abc4j.googlecode.com/svn/trunk@20 60e0f196-961e-0410-a92f-9d946f01d26a

--- scanner/NoSuchTokenException.java
+++
@@ -1,9 +0,0 @@
-package scanner;
-
-import java.lang.RuntimeException;
-
-/** A runtime exception thrown when no more token can be generated. */
-public class NoSuchTokenException extends RuntimeException
-{
-}
-
--- scanner/NoTransitionFoundException.java
+++
@@ -1,8 +0,0 @@
-package scanner;
-
-/** A runtime exception thrown when no transition is found when sending a
- * character in a finale state automata. */
-public class NoTransitionFoundException extends RuntimeException
-{
-}
-
